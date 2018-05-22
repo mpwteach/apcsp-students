91cbdedd26d57b4c090b10f16465415ed31a23df
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-05-22 07:42:03 -0700

    sizeof

--- cdemo/sizeofvariables.c
+++ cdemo/sizeofvariables.c
@@ -2,17 +2,9 @@
 
 int main()
 {
-  double a = 3.14159;
-  float b = 25.0;
-  int c = 545;
-  long int d = 123;
-  char e = 'B';
-  
-  printf("double a value %.4f and size %d bytes.\n", a, sizeof(a));
-  printf("float b value %4.2f and size %d bytes.\n", b, sizeof(b));
-  printf("int c value %d (oct %o, hex %x) and "		\
-	 "size %d bytes.\n", c, c, c, sizeof(c));
-  printf("long int d value %d and size %d bytes.\n", d, sizeof(d));
-  printf("char e value %c and size %d bytes.\n", e, sizeof(e));
+  int a = 545;
+
+  // print value and size of an int variable
+  printf("int a value %d (oct %o, hex %x) and size %d bytes.\n", c, c, c, sizeof(c));
 }
 
