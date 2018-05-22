1ee371035f5a5a2454a97dd4292a6e356405002f
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2018-05-22 07:43:16 -0700

    fix

--- cdemo/sizeofvariables.c
+++ cdemo/sizeofvariables.c
@@ -5,6 +5,6 @@ int main()
   int a = 545;
 
   // print value and size of an int variable
-  printf("int a value %d (oct %o, hex %x) and size %d bytes.\n", c, c, c, sizeof(c));
+  printf("int a value %d (oct %o, hex %x) and size %d bytes.\n", a, a, a, sizeof(a));
 }
 
