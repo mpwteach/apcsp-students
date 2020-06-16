1efff1b35dda56f4bdc5bb6e3d946dfd774fdd63
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2020-06-15 17:30:08 -0700

    update for tests

--- cdemo/ifstmnt.c
+++ cdemo/ifstmnt.c
@@ -1,17 +1,53 @@
 #include <stdio.h>
 
-int main()
+// for testing only - do not change
+void getTestInput(int argc, char* argv[], int* a, int* b)
 {
-  int a = 0;
+  if (argc == 3) {
+    sscanf(argv[1], "%d", a);
+    sscanf(argv[2], "%d", b);
+  }
+}
 
-  // if statement to test is a is equal to 0
-  if (a == 0)
-  {
-    printf("a is 0\n");
-  }
+
+int main(int argc, char* argv[]) 
+{
+  // the two variables to compare - change these values to test your code
+  int a = 5;
+  int b = 6;
+  
+  // for testing only - do not change
+  getTestInput(argc, argv, &a, &b);
+  
+  // equal operator - this one is done
+  if (a == b)
+    printf("true: %d == %d\n", a, b);
   else
-  {
-    printf("a is not 0\n");
-  }
+    printf("false: %d == %d\n", a, b);
     
+  // complete the rest below by adding if statements  
+    
+  // not equal operator
+  printf("true: %d != %d\n", a, b);
+  printf("false: %d != %d\n", a, b);
+  
+  // greater than
+  printf("true: %d > %d\n", a, b);
+  printf("false: %d > %d\n", a, b);
+    
+  // greater than or equal
+  printf("true: %d >= %d\n", a, b);
+  printf("false: %d >= %d\n", a, b);
+  
+  // and
+  printf("true: %d == 0 && %d == 0\n", a, b);
+  printf("false: %d == 0 && %d == 0\n", a, b);
+  
+  // or
+  printf("true: %d == 0 || %d == 0\n", a, b);
+  printf("false: %d == 0 || %d == 0\n", a, b);
+    
+  // not equal to 0
+  printf("true: !(%d == 0)\n", a);
+  printf("false: !(%d == 0)\n", a);
 }
---
+++ cdemo/iteration.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// for testing only - do not change
+void getTestInput(int argc, char* argv[], int* a)
+{
+  if (argc == 2) {
+    sscanf(argv[1], "%d", a);
+  }
+}
+
+
+int main(int argc, char* argv[]) 
+{
+  // the divisor variable
+  int div = 5;
+  
+  // for testing only - do not change
+  getTestInput(argc, argv, &div);
+
+
+  // add code below to complete 2.4 for any value of div 
+}
