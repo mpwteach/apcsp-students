32bc68c32e6b51772b2b886948c11e7b7296cfe2
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2021-01-07 02:44:18 +0000

    change

--- cdemo/arrayloop.c
+++ cdemo/arrayloop.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 // for testing only - do not change
-void getTestInput(int argc, char* argv[], int* a)
+void getTestInput(int argc, char* argv[], int* a, int* b)
 {
-  if (argc == 2) {
+  if (argc == 3) {
     sscanf(argv[1], "%d", a);
+    sscanf(argv[2], "%d", b);
   }
 }
 
@@ -17,12 +18,16 @@ void getTestInput(int argc, char* argv[], int* a)
 
 int main(int argc, char* argv[]) 
 {
+  // the variable which controls the size of the array
+  int size = 100;
+  
   // the variable which increments the values in the array
   int inc = 3;
   
   // for testing only - do not change
-  getTestInput(argc, argv, &inc);
+  getTestInput(argc, argv, &size, &inc);
 
+  printf("array loop size: %d, inc %d\n", size, inc);
   
   // add your code below to:
   // - initialize the a 100 element array
@@ -30,7 +35,7 @@ int main(int argc, char* argv[])
   // - pass the array to arrayAdd to add the increment of inc
   // - print out the array
 
-  printf("inc %d\n", inc);
+
 
 
   
