1dc92a02b291c72ba694fd28e008ea7f2c55e245
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2021-01-08 17:25:16 +0000

    comments

--- cdemo/arrayloop.c
+++ cdemo/arrayloop.c
@@ -30,7 +30,7 @@ int main(int argc, char* argv[])
   printf("array loop size: %d, inc %d\n", size, inc);
   
   // add your code below to:
-  // - initialize the a 100 element array
+  // - initialize the a 'size' element array
   // - fill it with the square of each index using a for loop i.e. 0, 1, 4, 9 etc
   // - pass the array to arrayAdd to add the increment of inc
   // - print out the array
