c0eb6edd06b9176f38a2d9174d7d0289b5ad6b90
Mark Whitehouse <mark.whitehouse@sduhsd.net>
2021-01-13 08:06:34 -0800

    comments

--- proj/student.c
+++ proj/student.c
@@ -12,38 +12,49 @@
 
 const char* STUFILE = "studentdata.txt";
 
-// holds the student pointer array
+// keeps track of the number of students in the students array
 int numStudents = 0;
+
+// holds the student pointer array
 Student* students[100] = {0};
 
 
 void createStudent(char* fname, char* lname, int age, int id)
 {
-
+  // createStudent dynamically creates a Student on the heap and adds that
+  // student to the student array
+  //  - the firstName and lastName strings should be dynamically created
+  //    based on the size of the fname and lname args
 }
 
 
 void deleteStudent(Student* student)
 {
-
+  // free the memory associated with a student including the strings
 }
 
 
 void deleteStudents()
 {
-
+  // iterate over the students array deleting every student and setting te pointer
+  // values to 0 and adjusting the numStudents to 0
 }
 
 
 void saveStudents(int key)
 {
-
+  // save all students in the student array to a file 'studentdata.txt' overwriting
+  // any existing file
+  //   - the format of the file is one line per student as follows fname lname age id:
+  //       tom thumb 15 1234 
+  //       james dean 21 2345 
+  //       katy jones 18 4532 
 }
 
 
 void loadStudents(int key)
 {
-
+  // load the students from the data file overwriting all exisiting students in memory
 }
 
 
@@ -64,9 +75,5 @@ void printStudents()
 }
 
 
-Student* getStudent(int num)
-{
-  return students[num];
-}
 
 
